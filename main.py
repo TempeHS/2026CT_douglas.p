da7c9ada7a70e4a01e4b986b7bc3738e2f1ce06e
douglas P <douglas.percival@education.nsw.gov.au>
2025-05-05 02:50:16 +0000

    add menu and template

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from flask import render_template
 
 app = Flask(__name__)
 
+@app.route('/index.html')
 @app.route('/')
-def hello_world():
-    backend_data = "douglas"
-    return render_template("index.html",front_end_data = backend_data), 200
+def index():
+    return render_template("index.html"), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
