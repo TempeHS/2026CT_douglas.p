421b421649d3c026b129321cde8cbd7c3b765b7d
douglas P <douglas.percival@education.nsw.gov.au>
2025-05-21 01:55:22 +0000

    added invisible cards

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 @app.route('/index.html')
 @app.route('/')
 def index():
+    card_data = (
+        ("The Talt","One of the first alien races humanity encountered", "come visit", "static/images/the talt.png"),
+        ("Star-Ships", "The wonderous star ships", "Take a look", "static/images/logo.png"),
+        ("Humainity","we were born on this rock, we wont die here ", "see how far we have come", "static/images/logo.png"),
+        
+    )
     return render_template("index.html"), 200
 
 if __name__ == '__main__':
