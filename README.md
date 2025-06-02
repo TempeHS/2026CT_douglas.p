7a714da129035e56cdd77f1fff9cfe31d1c6077b
douglas P <douglas.percival@education.nsw.gov.au>
2025-06-02 02:50:48 +0000

    button

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ git add -A
 git commit -m "add index"
 git push-
 to open website do python main.py
+to crash website and bring up a line in terminal do ctrl c
+to open or close terminal do ctrl ~
--- main.py
+++ main.py
@@ -8,11 +8,35 @@ app = Flask(__name__)
 def index():
     card_data = (
         ("The Talt","One of the first alien races humanity encountered", "come visit", "static/images/the talt.png"),
-        ("Star-Ships", "The wonderous star ships", "Take a look", "static/images/Untitled design.png"),
-        ("Humainity","we were born on this rock, we wont die here ", "see how far we have come", "static/images/Add a heading.png"),
-        
+        ("Star-Ships", "The wonderous star ships", "Take a look", "static/images/Spaceships.png"),
+        ("Humainity","we were born on this rock, we wont die here ", "see how far we have come", "static/images/we were born here.png"),
+        ("The Talt","One of the first alien races humanity encountered", "come visit", "static/images/the talt.png"),
+        ("Star-Ships", "The wonderous star ships", "Take a look", "static/images/Spaceships.png"),
+        ("Humainity","we were born on this rock, we wont die here ", "see how far we have come", "static/images/we were born here.png"),
     )
     return render_template("index.html", cards=card_data), 200
+
+@app.route('/contact.html')
+def contact():
+    return render_template("contact.html"), 200
+
+@app.route('/world1.html')
+def world1():
+    return render_template("world1.html"), 200
+
+@app.route('/world2.html')
+def world2():
+    return render_template("world2.html"), 200
+
+@app.route('/world3.html')
+def world3():
+    return render_template("world3.html"), 200
+
+@app.route('/world4.html')
+def world4():
+    return render_template("world4.html"), 200
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
