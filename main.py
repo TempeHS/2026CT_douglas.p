306c9901e06a5155692f9b6a877d540e72c59626
douglas P <douglas.percival@education.nsw.gov.au>
2025-06-27 02:27:03 +0000

    added mabye final feature

--- main.py
+++ main.py
@@ -9,44 +9,64 @@ def aboutthesite():
     card_data = (
         ("The Talt","Come meet one of the first alien races humanity encountered", "visit", "static/images/the talt.png", "/talt.html"),
         ("Star-Ships", "The wonderous star ships", "Take a look", "static/images/Spaceships.png", "/starships.html"),
-        ("Humainity","we were born on this rock, we wont die here ", "see how far we have come", "static/images/we were born here.png", "/humanity.html"),
+        ("Humanity","we were born on this rock, we wont die here ", "see how far we have come", "static/images/we were born here.png", "/humanity.html"),
     )
     return render_template("aboutthesite.html", cards=card_data), 200
 
 @app.route('/contact.html')
 def contact():
     return render_template("contact.html"), 200
+
 @app.route('/earth.html')
 def earth():
-    return render_template("earth.html"), 200
+    headings = [
+        ("intro", "Title"),
+        ("Heading-two", "Heading two"),
+    ]
+    return render_template("earth.html", active_page="earth", headings=headings), 200
 
 @app.route('/fartworld.html')
 def fartworld():
-    return render_template("fartworld.html"), 200
+    return render_template("fartworld.html", active_page="fartworld"), 200
 
 @app.route('/redworld.html')
 def redworld():
-    return render_template("redworld.html"), 200
+    return render_template("redworld.html", active_page="redworld"), 200
 
 @app.route('/skult.html')
 def skult():
-    return render_template("skult.html"), 200
+    headings = [
+        ("intro", "Title"),
+        ("Heading-two", "skult location"),
+    ]
+    return render_template("skult.html", active_page="skult", headings=headings), 200
 
 @app.route('/generalinfo.html')
 def generalinfo():
-    return render_template("generalinfo.html"), 200
+    return render_template("generalinfo.html", active_page="generalinfo"), 200
 
 @app.route('/talt.html')
 def talt():
-    return render_template("talt.html"), 200
+    headings = [
+        ("intro", "Title"),
+        ("Heading-two", "Talt biology"),
+        ("Heading-three", "Early Talt technology"),
+    ]
+    return render_template("talt.html", active_page="talt", headings=headings), 200
 
 @app.route('/starships.html')
 def starships():
-    return render_template("starships.html"), 200
+    return render_template("starships.html", active_page="starships",), 200
 
 @app.route('/humanity.html')
 def humanity():
-    return render_template("humanity.html"), 200
+    headings = [
+        ("intro", "Title"),
+        ("Heading-two", "early space age"),
+        ("Heading-three", "first contact"),
+        ("Heading-four", "where are we now?"),
+    ]
+    return render_template("humanity.html", active_page="humanity",headings=headings), 200
 
 
 if __name__ == '__main__':
